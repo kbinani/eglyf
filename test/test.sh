2c3b4b9b2e69086bbf3a82118952772c5b955089
kbinani <kbinani.bt@gmail.com>
2025-03-12 16:53:04 +0900

    feat: Testing tables in parallel

--- test/test.sh
+++ test/test.sh
@@ -8,20 +8,48 @@ extension="${filename##*.}"
 name="${filename%.*}"
 output="$dir/$name-out.$extension"
 
-"$resave" "$input" "$output"
-
 input_ttx="$dir/$name.ttx"
 output_ttx="$dir/$name-out.ttx"
 
-ttx -q -o "$input_ttx" "$input"
+ttx -q -o "$input_ttx" "$input" &
+
+"$resave" "$input" "$output" &
+
+wait
+
+if [ ! -f "$output" ]; then
+  exit 255
+fi
+
 ttx -q -o "$output_ttx" "$output"
 
 tables=$(xmlstarlet sel -t -m "ttFont/*" -n  -v "name()" "$input_ttx" | grep -v DSIG)
 
+function test {
+  table="$1"
+  tmp=$(mktemp)
+  xmlstarlet sel -t -c "ttFont/$table" "$input_ttx" | grep -v numberOfHMetrics > "${table}-expected.xml"
+  xmlstarlet sel -t -c "ttFont/$table" "$output_ttx" | grep -v numberOfHMetrics > "${table}-actual.xml"
+
+  if diff -u "${table}-expected.xml" "${table}-actual.xml" > "$tmp"; then
+    echo -n
+  else
+    cat "$tmp" | head -30
+    exit 255
+  fi
+  rm -f "${table}-expected.xml" "${table}-actual.xml" "$tmp"
+}
+
+pids=()
 for table in $tables; do
-  echo "table='$table'"
-  xmlstarlet sel -t -c "ttFont/$table" "$input_ttx" > "${table}-expected.xml"
-  xmlstarlet sel -t -c "ttFont/$table" "$output_ttx" > "${table}-actual.xml"
-  diff -u "${table}-expected.xml" "${table}-actual.xml"
-  rm -f "${table}-expected.xml" "${table}-actual.xml"
+  test "$table" &
+  pids+=($!)
+done
+
+for pid in ${pids[@]}; do
+  wait $pid
+  if [ $? -ne 0 ]; then
+    wait
+    exit 3
+  fi
 done
