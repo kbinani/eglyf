76726ebab27ab128e571671551288288808a901c
kbinani <kbinani.bt@gmail.com>
2025-03-17 07:59:08 +0900

    feat: Encode mark to ligature attachment positioning

--- src/table/gpos/MarkToLigatureAttachmentPositioning.hpp
+++ src/table/gpos/MarkToLigatureAttachmentPositioning.hpp
@@ -6,6 +6,16 @@ class MarkToLigatureAttachmentPositioning : public Subtable {
 public:
   struct ComponentRecord {
     std::vector<std::shared_ptr<Anchor>> ligatureAnchors;
+
+    size_t size() const {
+      size_t ret = ligatureAnchors.size() * sizeof(Offset16);
+      for (auto const &anchor : ligatureAnchors) {
+        if (anchor) {
+          ret += anchor->size();
+        }
+      }
+      return ret;
+    }
   };
 
   struct LigatureAttach {
@@ -42,6 +52,58 @@ public:
       }
       return Status::Ok();
     }
+
+    Status write(OutputStream &out, uint16_t markClassCount) const {
+      using namespace std;
+      auto writer = make_shared<OffsetWriter>(out);
+      if (!out.sizeU16(componentRecords.size())) {
+        return EGLYF_ERROR;
+      }
+      vector<vector<OffsetWriter::Handle16>> ligatureAnchorOffsetsList;
+      for (auto const &record : componentRecords) {
+        vector<OffsetWriter::Handle16> offsets;
+        if (record.ligatureAnchors.size() != markClassCount) {
+          return EGLYF_ERROR;
+        }
+        for (uint16_t i = 0; i < markClassCount; i++) {
+          auto offset = writer->o16();
+          if (!offset) {
+            return EGLYF_ERROR;
+          }
+          offsets.push_back(offset);
+        }
+        ligatureAnchorOffsetsList.push_back(offsets);
+      }
+      for (size_t i = 0; i < componentRecords.size(); i++) {
+        auto const &record = componentRecords[i];
+        auto const &offsets = ligatureAnchorOffsetsList[i];
+        for (size_t j = 0; j < record.ligatureAnchors.size(); j++) {
+          auto const &anchor = record.ligatureAnchors[j];
+          auto offset = offsets[j];
+          if (anchor) {
+            if (auto st = offset->mark(); !st.ok()) {
+              return EGLYF_STATUS_PUSH(st);
+            }
+            if (auto st = anchor->write(out); !st.ok()) {
+              return EGLYF_STATUS_PUSH(st);
+            }
+          } else {
+            if (auto st = offset->null(); !st.ok()) {
+              return EGLYF_STATUS_PUSH(st);
+            }
+          }
+        }
+      }
+      return EGLYF_STATUS_PUSH(writer->commit());
+    }
+
+    size_t size() const {
+      size_t ret = sizeof(uint16_t);
+      for (auto const &record : componentRecords) {
+        ret += record.size();
+      }
+      return ret;
+    }
   };
 
   struct LigatureArray {
@@ -70,6 +132,41 @@ public:
       }
       return Status::Ok();
     }
+
+    Status write(OutputStream &out, uint16_t markClassCount) const {
+      using namespace std;
+      auto writer = make_shared<OffsetWriter>(out);
+      if (!out.sizeU16(ligatureAttaches.size())) {
+        return EGLYF_ERROR;
+      }
+      vector<OffsetWriter::Handle16> ligatureAttachOffsets;
+      for (size_t i = 0; i < ligatureAttaches.size(); i++) {
+        auto offset = writer->o16();
+        if (!offset) {
+          return EGLYF_ERROR;
+        }
+        ligatureAttachOffsets.push_back(offset);
+      }
+      for (size_t i = 0; i < ligatureAttaches.size(); i++) {
+        auto const &ligatureAttach = ligatureAttaches[i];
+        auto offset = ligatureAttachOffsets[i];
+        if (auto st = offset->mark(); !st.ok()) {
+          return EGLYF_STATUS_PUSH(st);
+        }
+        if (auto st = ligatureAttach.write(out, markClassCount); !st.ok()) {
+          return EGLYF_STATUS_PUSH(st);
+        }
+      }
+      return EGLYF_STATUS_PUSH(writer->commit());
+    }
+
+    size_t size() const {
+      size_t ret = sizeof(uint16_t) + ligatureAttaches.size() * sizeof(Offset16);
+      for (auto const &attach : ligatureAttaches) {
+        ret += attach.size();
+      }
+      return ret;
+    }
   };
 
 public:
@@ -91,8 +188,8 @@ public:
     if (!in.o16(&ligatureCoverageOffset)) {
       return EGLYF_ERROR;
     }
-    uint16_t markClassCount;
-    if (!in.u16(&markClassCount)) {
+    auto ret = make_unique<MarkToLigatureAttachmentPositioning>();
+    if (!in.u16(&ret->markClassCount)) {
       return EGLYF_ERROR;
     }
     Offset16 markArrayOffset;
@@ -103,8 +200,6 @@ public:
     if (!in.o16(&ligatureArrayOffset)) {
       return EGLYF_ERROR;
     }
-
-    auto ret = make_unique<MarkToLigatureAttachmentPositioning>();
 
     if (!in.seek(markCoverageOffset)) {
       return EGLYF_ERROR;
@@ -120,21 +215,17 @@ public:
       return EGLYF_STATUS_PUSH(st);
     }
 
-    {
-      if (!in.seek(markArrayOffset)) {
-        return EGLYF_ERROR;
-      }
-      if (auto st = MarkArray::Read(in, ret->markArray); !st.ok()) {
-        return EGLYF_STATUS_PUSH(st);
-      }
+    if (!in.seek(markArrayOffset)) {
+      return EGLYF_ERROR;
     }
-    {
-      if (!in.seek(ligatureArrayOffset)) {
-        return EGLYF_ERROR;
-      }
-      if (auto st = LigatureArray::Read(in, ret->ligatureArray, markClassCount); !st.ok()) {
-        return EGLYF_STATUS_PUSH(st);
-      }
+    if (auto st = MarkArray::Read(in, ret->markArray); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+    if (!in.seek(ligatureArrayOffset)) {
+      return EGLYF_ERROR;
+    }
+    if (auto st = LigatureArray::Read(in, ret->ligatureArray, ret->markClassCount); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
     }
 
     out.reset(ret.release());
@@ -142,16 +233,75 @@ public:
   }
 
   Status write(OutputStream &out, std::map<std::shared_ptr<Subtable>, std::pair<std::shared_ptr<OffsetWriter>, OffsetWriter::Handle32>> &extensions) override {
-    return EGLYF_ERROR;
+    using namespace std;
+    auto writer = make_shared<OffsetWriter>(out);
+    if (!out.u16(1)) {
+      return EGLYF_ERROR;
+    }
+    auto markCoverageOffset = writer->o16();
+    if (!markCoverageOffset) {
+      return EGLYF_ERROR;
+    }
+    auto ligatureCoverageOffset = writer->o16();
+    if (!ligatureCoverageOffset) {
+      return EGLYF_ERROR;
+    }
+    if (!out.u16(markClassCount)) {
+      return EGLYF_ERROR;
+    }
+    auto markArrayOffset = writer->o16();
+    if (!markArrayOffset) {
+      return EGLYF_ERROR;
+    }
+    auto ligatureArrayOffset = writer->o16();
+    if (!ligatureArrayOffset) {
+      return EGLYF_ERROR;
+    }
+
+    if (auto st = markCoverageOffset->mark(); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+    if (auto st = markCoverage->write(out); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+
+    if (auto st = ligatureCoverageOffset->mark(); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+    if (auto st = ligatureCoverage->write(out); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+
+    if (auto st = markArrayOffset->mark(); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+    if (auto st = markArray.write(out); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+
+    if (auto st = ligatureArrayOffset->mark(); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+    if (auto st = ligatureArray.write(out, markClassCount); !st.ok()) {
+      return EGLYF_STATUS_PUSH(st);
+    }
+
+    return EGLYF_STATUS_PUSH(writer->commit());
   }
 
   size_t size() const override {
-    return 0;
+    size_t ret = 2 * sizeof(uint16_t) + 4 * sizeof(Offset16);
+    ret += markCoverage->size();
+    ret += ligatureCoverage->size();
+    ret += markArray.size();
+    ret += ligatureArray.size();
+    return ret;
   }
 
 public:
   std::shared_ptr<Coverage> markCoverage;
   std::shared_ptr<Coverage> ligatureCoverage;
+  uint16_t markClassCount;
   MarkArray markArray;
   LigatureArray ligatureArray;
 };
