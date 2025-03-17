5a8a1f1bb85146902edbe977df459d94b5512670
kbinani <kbinani.bt@gmail.com>
2025-03-17 21:04:00 +0900

    fix: Offset zero when reading reverse chained contexts single

--- src/table/gsub/ReverseChainedContextsSingle.hpp
+++ src/table/gsub/ReverseChainedContextsSingle.hpp
@@ -4,8 +4,9 @@ namespace eglyf::gsub {
 
 class ReverseChainedContextsSingle : public Subtable {
 public:
-  static Status Read(InputStream &in, std::shared_ptr<Subtable> &out) {
+  static Status Read(InputStream &stream, std::shared_ptr<Subtable> &out) {
     using namespace std;
+    OffsetInputStream in(&stream);
     uint16_t format;
     if (!in.u16(&format)) {
       return EGLYF_ERROR;
