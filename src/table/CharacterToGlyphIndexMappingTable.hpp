1ac75066e7f78803529b0ee01a7e99ea9e01caa3
kbinani <kbinani.bt@gmail.com>
2025-03-22 09:41:50 +0900

    feat: Parse cmap subtable format 12

--- src/table/CharacterToGlyphIndexMappingTable.hpp
+++ src/table/CharacterToGlyphIndexMappingTable.hpp
@@ -106,6 +106,64 @@ public:
     std::vector<uint16_t> glyphIdArray;
   };
 
+  class SegmentedCoverage : public CmapSubtable {
+  public:
+    struct SequentialMapGroup {
+      uint32_t startCharCode;
+      uint32_t endCharCode;
+      uint32_t startGlyphID;
+    };
+
+  public:
+    static Status Read(InputStream &stream, std::shared_ptr<CmapSubtable> &out) {
+      using namespace std;
+      uint16_t reserved;
+      if (!stream.u16(&reserved)) {
+        return EGLYF_ERROR;
+      }
+      uint32_t length;
+      if (!stream.u32(&length)) {
+        return EGLYF_ERROR;
+      }
+      if (length < 8) {
+        return EGLYF_ERROR;
+      }
+      string data;
+      data.resize(length - 8);
+      if (!stream.read(data.data(), data.size())) {
+        return EGLYF_ERROR;
+      }
+      auto ret = make_unique<SegmentedCoverage>();
+      ByteInputStream in(data);
+      if (!in.u32(&ret->language)) {
+        return EGLYF_ERROR;
+      }
+      uint32_t numGroups;
+      if (!in.u32(&numGroups)) {
+        return EGLYF_ERROR;
+      }
+      for (uint32_t i = 0; i < numGroups; i++) {
+        SequentialMapGroup g;
+        if (!in.u32(&g.startCharCode)) {
+          return EGLYF_ERROR;
+        }
+        if (!in.u32(&g.endCharCode)) {
+          return EGLYF_ERROR;
+        }
+        if (!in.u32(&g.startGlyphID)) {
+          return EGLYF_ERROR;
+        }
+        ret->groups.push_back(g);
+      }
+      out.reset(ret.release());
+      return Status::Ok();
+    }
+
+  public:
+    uint32_t language;
+    std::vector<SequentialMapGroup> groups;
+  };
+
 public:
   static Status Read(InputStream &stream, std::shared_ptr<CharacterToGlyphIndexMappingTable> &out) {
     using namespace std;
@@ -168,6 +226,12 @@ public:
         }
       case 4:
         if (auto st = SegmentMappingToDeltaValues::Read(in, subtable); !st.ok()) {
+          return EGLYF_STATUS_PUSH(st);
+        } else {
+          break;
+        }
+      case 12:
+        if (auto st = SegmentedCoverage::Read(in, subtable); !st.ok()) {
           return EGLYF_STATUS_PUSH(st);
         } else {
           break;
