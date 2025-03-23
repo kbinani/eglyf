cbe62aa20eaa98f4d9aca8310c4d45910e8ea707
kbinani <kbinani.bt@gmail.com>
2025-03-23 11:48:42 +0900

    feat: Use juce::ConsoleApplication class

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,46 @@
 #include "eglyf.hpp"
 
-int main() {
+#include <juce_core/juce_core.h>
+
+static void Run(juce::ArgumentList const &args) {
+  using namespace std;
   using namespace eglyf;
-  FileInputStream fis(juce::File::getCurrentWorkingDirectory().getChildFile("eot.ttf")); // egyptiantext-COLR.ttf"));
-  auto ff = FontFile::Read(fis);
-  if (!ff) {
-    return 1;
+
+  juce::File input = args.getExistingFileForOption("--input");
+  juce::File output = args.getFileForOption("--output");
+
+  FileInputStream fis(input);
+  shared_ptr<FontFile> ff;
+  if (auto st = FontFile::Read(fis, ff); !st.ok()) {
+    stringstream out;
+    st.print(out);
+    juce::ConsoleApplication::fail(juce::String(out.str()));
+    return;
   }
   auto gid0 = ff->addEmptyGlyph("foo", 0, 0);
   if (!gid0) {
-    return 1;
+    juce::ConsoleApplication::fail(juce::String("addEmptyGlyph failed"));
+    return;
   }
   auto gid1 = ff->addCompositeGlyph("baz", GlyphDataTable::CompositeGlyph::GlyphRecord::New(1, 0, 0), 0, 0);
   if (!gid1) {
-    return 1;
+    juce::ConsoleApplication::fail(juce::String("addCompositeGlyph failed"));
+    return;
   }
-  FileOutputStream fos(juce::File::getCurrentWorkingDirectory().getChildFile("out.ttf"));
-  if (ff->write(fos)) {
-    return 0;
-  } else {
-    return 1;
+  FileOutputStream fos(output);
+  if (auto st = ff->write(fos); !st.ok()) {
+    stringstream out;
+    st.print(out);
+    juce::ConsoleApplication::fail(juce::String(out.str()));
   }
+}
+
+int main(int argc, char *argv[]) {
+  juce::ConsoleApplication app;
+  app.addDefaultCommand({juce::String("--run"),
+                         juce::String("--run filename"),
+                         juce::String(""),
+                         juce::String(""),
+                         [](auto const &args) { Run(args); }});
+  return app.findAndRunCommand(argc, argv);
 }
