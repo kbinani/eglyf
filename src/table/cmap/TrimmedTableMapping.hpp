b502116118f658d26ce68d15e7c081a664008713
kbinani <kbinani.bt@gmail.com>
2025-03-26 13:58:43 +0900

    feat: Implement TrimmedTableMapping::map

--- src/table/cmap/TrimmedTableMapping.hpp
+++ src/table/cmap/TrimmedTableMapping.hpp
@@ -58,7 +58,32 @@ public:
   }
 
   Status map(uint32_t codepoint, uint16_t glyphId) {
-    return EGLYF_ERROR;
+    using namespace std;
+    if (codepoint > 0xffff) {
+      return EGLYF_ERROR;
+    }
+    if (codepoint < firstCode) {
+      int num = firstCode - codepoint;
+      glyphIdArray.resize(glyphIdArray.size() + num);
+      for (int i = glyphIdArray.size(); i > num; i--) {
+        glyphIdArray[i] = glyphIdArray[i - num];
+      }
+      glyphIdArray[0] = glyphId;
+      for (int i = 1; i < num; i++) {
+        glyphIdArray[i] = 0;
+      }
+      firstCode = codepoint;
+      return Status::Ok();
+    }
+    size_t index = codepoint - firstCode;
+    if (index < glyphIdArray.size()) {
+      glyphIdArray[index] = glyphId;
+      return Status::Ok();
+    } else {
+      glyphIdArray.resize(index + 1, 0);
+      glyphIdArray[index] = glyphId;
+      return Status::Ok();
+    }
   }
 
 public:
