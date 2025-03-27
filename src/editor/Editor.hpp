af38a9cf22aec4d7866e2ac5351406431e3c5a62
kbinani <kbinani.bt@gmail.com>
2025-03-27 22:30:02 +0900

    feat: Add Glyph struct

--- src/editor/Editor.hpp
+++ src/editor/Editor.hpp
@@ -4,8 +4,13 @@ namespace eglyf {
 
 class Editor : public std::enable_shared_from_this<Editor> {
 public:
+  struct Glyph {
+    std::optional<uint16_t> id;
+    GlyphDefinitionTable::Class classDef;
+  };
+
   struct Group {
-    std::vector<std::variant<std::shared_ptr<Group>, uint16_t>> members;
+    std::vector<std::variant<std::shared_ptr<Group>, std::shared_ptr<Glyph>>> members;
   };
 
   struct GroupBuilder {
@@ -36,9 +41,8 @@ public:
     GroupBuilder *addGlyph(std::string const &name) {
       auto e = editor.lock();
       if (e) {
-        if (auto glyphId = e->getGlyphByName(name); glyphId) {
-          group->members.push_back(*glyphId);
-        }
+        auto g = e->getGlyphByName(name);
+        group->members.push_back(g);
       }
       return this;
     }
@@ -51,48 +55,12 @@ public:
   explicit Editor(std::shared_ptr<FontFile> const &font) : font(font) {
   }
 
-  Optional<uint16_t> defineGlyph(std::string const &name, std::optional<uint32_t> unicode, GlyphDefinitionTable::Class classDef) {
-    using namespace std;
-    uint16_t glyphId = 0;
-    if (auto gid = font->post->getGlyphId(name); gid) {
-      glyphId = *gid;
-    } else {
-      if (auto gid = font->addEmptyGlyph(name, 0, 0); gid) {
-        glyphId = *gid;
-      } else {
-        return EGLYF_NULLOPT_PUSH(gid.status());
-      }
-    }
-    glyphs[name] = glyphId;
-    if (unicode) {
-      if (auto st = font->cmap->map(*unicode, glyphId); !st.ok()) {
-        return EGLYF_NULLOPT_PUSH(st);
-      }
-    }
-    if (!font->gdef) {
-      font->gdef = make_shared<GlyphDefinitionTable>();
-      font->gdef->majorVersion = 1;
-      font->gdef->minorVersion = 2;
-    }
-    if (!font->gdef->glyphClassDef) {
-      font->gdef->glyphClassDef = make_shared<ClassDef2>();
-    }
-    if (auto st = font->gdef->glyphClassDef->add(glyphId, static_cast<uint16_t>(classDef)); !st.ok()) {
-      return EGLYF_NULLOPT_PUSH(st);
-    }
-    return glyphId;
-  }
-
-  std::shared_ptr<GroupBuilder> defineGroup(std::string const &name) {
-    using namespace std;
-    auto g = getGroupByName(name);
-    return make_shared<GroupBuilder>(shared_from_this(), g);
-  }
-
-  std::optional<uint16_t> getGlyphByName(std::string const &name) {
+  std::shared_ptr<Glyph> getGlyphByName(std::string const &name) {
     using namespace std;
     if (auto found = glyphs.find(name); found == glyphs.end()) {
-      return nullopt;
+      auto g = make_shared<Glyph>();
+      glyphs[name] = g;
+      return g;
     } else {
       return found->second;
     }
@@ -116,9 +84,50 @@ public:
     return Status::Ok();
   }
 
+private:
+  Optional<uint16_t> defineGlyph(std::string const &name, std::optional<uint32_t> unicode, GlyphDefinitionTable::Class classDef) {
+    using namespace std;
+    auto g = getGlyphByName(name);
+    uint16_t glyphId = 0;
+    if (auto gid = font->post->getGlyphId(name); gid) {
+      glyphId = *gid;
+    } else {
+      if (auto gid = font->addEmptyGlyph(name, 0, 0); gid) {
+        glyphId = *gid;
+      } else {
+        return EGLYF_NULLOPT_PUSH(gid.status());
+      }
+    }
+    if (unicode) {
+      if (auto st = font->cmap->map(*unicode, glyphId); !st.ok()) {
+        return EGLYF_NULLOPT_PUSH(st);
+      }
+    }
+    if (!font->gdef) {
+      font->gdef = make_shared<GlyphDefinitionTable>();
+      font->gdef->majorVersion = 1;
+      font->gdef->minorVersion = 2;
+    }
+    if (!font->gdef->glyphClassDef) {
+      font->gdef->glyphClassDef = make_shared<ClassDef2>();
+    }
+    if (auto st = font->gdef->glyphClassDef->add(glyphId, static_cast<uint16_t>(classDef)); !st.ok()) {
+      return EGLYF_NULLOPT_PUSH(st);
+    }
+    g->id = glyphId;
+    g->classDef = classDef;
+    return glyphId;
+  }
+
+  std::shared_ptr<GroupBuilder> defineGroup(std::string const &name) {
+    using namespace std;
+    auto g = getGroupByName(name);
+    return make_shared<GroupBuilder>(shared_from_this(), g);
+  }
+
 public:
   std::shared_ptr<FontFile> font;
-  std::unordered_map<std::string, uint16_t> glyphs;
+  std::unordered_map<std::string, std::shared_ptr<Glyph>> glyphs;
   std::unordered_map<std::string, std::shared_ptr<Group>> groups;
 };
 
