8c3e44485a5af35d656ae95b94c6c6b5e1f91b6d
kbinani <kbinani.bt@gmail.com>
2025-04-24 03:28:26 +0900

    fix: Fix doc comment for t

--- src/editor/PlaceholderGlyph.hpp
+++ src/editor/PlaceholderGlyph.hpp
@@ -24,7 +24,7 @@ class PlaceholderGlyph {
   }
 
   struct CommonParam {
-    int t;       // line width
+    int t;       // (line width) / 2
     int segment; // base length of (1 dash + 1 gap)
   };
 
@@ -39,7 +39,6 @@ class PlaceholderGlyph {
 
 public:
   static Rect<int16_t> Bounds(int h, int v, int16_t base, int16_t hfu, int chu, int16_t vfu, int vhu) {
-    using namespace std;
     CommonParam c = CommonParameter(chu, hfu, vhu, vfu);
     Param x = Parameter(h, c.segment, hfu);
     Param y = Parameter(v, c.segment, vfu);
@@ -51,71 +50,68 @@ public:
     using Contour = glyf::GlyphDataTable::Contour;
     using Point = glyf::GlyphDataTable::Point;
     using SimpleGlyph = glyf::GlyphDataTable::SimpleGlyph;
+
     auto &outlines = font.outlines;
     if (!holds_alternative<FontFile::TrueTypeOutlines>(outlines)) {
       return EGLYF_ERROR;
     }
     auto &glyf = get<FontFile::TrueTypeOutlines>(outlines).glyf;
     CommonParam p = CommonParameter(chu, hfu, vhu, vfu);
-    int const segment = p.segment;
-    int const t = p.t;
 
     for (int xLevel = 1; xLevel <= chu; xLevel++) {
-      Param x = Parameter(xLevel, segment, hfu);
-      int const w = x.s;
+      Param x = Parameter(xLevel, p.segment, hfu);
       for (int yLevel = 1; yLevel <= vhu; yLevel++) {
-        Param y = Parameter(yLevel, segment, vfu);
-        int const h = y.s;
+        Param y = Parameter(yLevel, p.segment, vfu);
         auto name = format("GB1_{}{}", xLevel, yLevel);
         vector<Contour> half;
         for (int i = x.n / 2; i < x.n - 1; i++) {
           Contour c;
-          c.points.emplace_back(-w + (x.e + x.d) * i, h + t);
-          c.points.emplace_back(-w + (x.e + x.d) * i + x.e, h + t);
-          c.points.emplace_back(-w + (x.e + x.d) * i + x.e, h - t);
-          c.points.emplace_back(-w + (x.e + x.d) * i, h - t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i, y.s + p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i + x.e, y.s + p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i + x.e, y.s - p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i, y.s - p.t);
           half.push_back(c);
         }
         {
           // top right corner
           Contour c;
-          c.points.emplace_back(w - x.e, h + t);
-          c.points.emplace_back(w + t, h + t);
-          c.points.emplace_back(w + t, h - y.e);
-          c.points.emplace_back(w - t, h - y.e);
-          c.points.emplace_back(w - t, h - t);
-          c.points.emplace_back(w - x.e, h - t);
+          c.points.emplace_back(x.s - x.e, y.s + p.t);
+          c.points.emplace_back(x.s + p.t, y.s + p.t);
+          c.points.emplace_back(x.s + p.t, y.s - y.e);
+          c.points.emplace_back(x.s - p.t, y.s - y.e);
+          c.points.emplace_back(x.s - p.t, y.s - p.t);
+          c.points.emplace_back(x.s - x.e, y.s - p.t);
           half.push_back(c);
         }
         for (int i = 1; i < y.n - 1; i++) {
           Contour c;
-          c.points.emplace_back(w + t, h - (y.e + y.d) * i);
-          c.points.emplace_back(w + t, h - (y.e + y.d) * i - y.e);
-          c.points.emplace_back(w - t, h - (y.e + y.d) * i - y.e);
-          c.points.emplace_back(w - t, h - (y.e + y.d) * i);
+          c.points.emplace_back(x.s + p.t, y.s - (y.e + y.d) * i);
+          c.points.emplace_back(x.s + p.t, y.s - (y.e + y.d) * i - y.e);
+          c.points.emplace_back(x.s - p.t, y.s - (y.e + y.d) * i - y.e);
+          c.points.emplace_back(x.s - p.t, y.s - (y.e + y.d) * i);
           half.push_back(c);
         }
         {
           // bottom right corner
           Contour c;
-          c.points.emplace_back(w + t, -h + y.e);
-          c.points.emplace_back(w + t, -h - t);
-          c.points.emplace_back(w - x.e, -h - t);
-          c.points.emplace_back(w - x.e, -h + t);
-          c.points.emplace_back(w - t, -h + t);
-          c.points.emplace_back(w - t, -h + y.e);
+          c.points.emplace_back(x.s + p.t, -y.s + y.e);
+          c.points.emplace_back(x.s + p.t, -y.s - p.t);
+          c.points.emplace_back(x.s - x.e, -y.s - p.t);
+          c.points.emplace_back(x.s - x.e, -y.s + p.t);
+          c.points.emplace_back(x.s - p.t, -y.s + p.t);
+          c.points.emplace_back(x.s - p.t, -y.s + y.e);
           half.push_back(c);
         }
-        for (int i = x.n - 2; i >= (x.n % 2 == 0 ? x.n / 2 : x.n / 2 + 1); i--) {
+        for (int i = x.n - 2; i >= (x.n + 1) / 2; i--) {
           Contour c;
-          c.points.emplace_back(-w + (x.e + x.d) * i, -h + t);
-          c.points.emplace_back(-w + (x.e + x.d) * i + x.e, -h + t);
-          c.points.emplace_back(-w + (x.e + x.d) * i + x.e, -h - t);
-          c.points.emplace_back(-w + (x.e + x.d) * i, -h - t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i, -y.s + p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i + x.e, -y.s + p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i + x.e, -y.s - p.t);
+          c.points.emplace_back(-x.s + (x.e + x.d) * i, -y.s - p.t);
           half.push_back(c);
         }
         int x0 = 0;
-        int y0 = base + h;
+        int y0 = base + y.s;
         vector<Contour> contours;
         for (auto const &c : half) {
           Contour cp;
@@ -131,7 +127,7 @@ public:
           }
           contours.push_back(cp);
         }
-        auto gid = font.addSimpleGlyph(name, gdef::GlyphDefinitionTable::Class::Mark, contours, 0, -w - t);
+        auto gid = font.addSimpleGlyph(name, gdef::GlyphDefinitionTable::Class::Mark, contours, 0, -(x.s + p.t));
         if (!gid) {
           return EGLYF_STATUS_PUSH(gid.status());
         }
