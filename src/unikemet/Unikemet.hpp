23055de06e101284a83e5138d4a5f79ebb3e6508
kbinani <kbinani.bt@gmail.com>
2025-12-02 06:16:29 +0900

    feat: Add custom AltSeq for M17A U+131CC

--- src/unikemet/Unikemet.hpp
+++ src/unikemet/Unikemet.hpp
@@ -11,6 +11,7 @@ public:
     static auto const create = []() -> unordered_map<uint32_t, vector<uint32_t>> * {
       auto p = new unordered_map<uint32_t, vector<uint32_t>>();
       uint32_t D50 = 0x130ad;
+      uint32_t M17 = 0x131cb;
       uint32_t hj = 0x13431;
       uint32_t vj = 0x13430;
       unordered_map<uint32_t, vector<uint32_t>> &r = *p;
@@ -24,6 +25,7 @@ public:
       r[0x130b4] = {D50, hj, D50, hj, D50, hj, D50, vj, D50, hj, D50, hj, D50, hj, D50};          // D50G 𓂴
       r[0x130b5] = {D50, hj, D50, hj, D50, vj, D50, hj, D50, hj, D50, vj, D50, hj, D50, hj, D50}; // D50H 𓂵
       r[0x130b6] = {D50, hj, D50, hj, D50, hj, D50, hj, D50};                                     // D50I 𓂶
+      r[0x131cc] = {M17, hj, M17};
       return p;
     };
     static unique_ptr<unordered_map<uint32_t, vector<uint32_t>> const> const sTable(create());
